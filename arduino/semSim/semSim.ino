59b2e63003f686fd8c8561bd1e79237cef7f8496
Bret Comnes <bcomnes@gmail.com>
2014-08-17 15:18:15 -0700

    Have a basic SEM sim working

--- arduino/semSim/semSim.ino
+++ arduino/semSim/semSim.ino
@@ -3,10 +3,6 @@
 
   http://www.arduino.cc/en/Tutorial/SerialEvent
 
-  (Line feed, '\n', 0x0A, 10 in decimal <----
-  CR (Carriage return, '\r', 0x0D, 13 in decimal)
-  (CR+LF, '\r\n', 0x0D0A)
-
   **************
   Command Format
   **************
@@ -17,18 +13,35 @@
     <CR>: Carriage return code; a terminator signifying the end of a command
     _: Space code: used to separate between the command and argument or between arguments
 
+  *******************
+  Receiving a command
+  *******************
+
+  !A[_OP value]<CR>
+
+  *************
+  Special Chars
+  *************
+
+  <LF> ('\n') (Line feed, '\n', 0x0A, 10 in decimal <----
+  <CR> ('\r') (Carriage return, '\r', 0x0D, 13 in decimal)
+  (CR+LF, '\r\n', 0x0D0A)
 
  */
 
 String inputCommand = ""; // A string to capture the command before the space
 String inputValue = "";  // A string to capture command value after space
 String inputString = ""; // The raw Readline string
+String testString = ""; // Used for string testing/
+String simVar = "10"; // TODO: Set up a larger bank of sim variables
 
 boolean stringComplete = false;  // whether the string is complete
 boolean separator = false; // When a space is received
 
 int led = 13; // An indication LED Pin
-int res; // Response status
+int res = 1; // Response status.  res status 1 is unhandled
+int val = false; // a value has been parsed
+int com = false; // a command has been parsed
 
 void setup() {
   // initialize serial:
@@ -37,6 +50,7 @@ void setup() {
   inputString.reserve(200); // Who knows
   inputCommand.reserve(20);  // These need to be short
   inputValue.reserve(20); //TODO: Stop appending if to many chars a rerecived.
+  simVar.reserve(20);
   digitalWrite(led, LOW);
 }
 
@@ -44,40 +58,68 @@ void loop() {
   // print the string when a newline arrives:
   if (stringComplete) {
     digitalWrite(led, LOW); // Turn off the LED.  We are processing now
-    // Stuff
-    //Serial.print(inputString);
-    //Serial.println(inputCommand);
-    //Serial.println(inputValue);
+
+    if (val) {
+      simVar = inputValue; // Update the global sim variable
+    }
+
+    // Simple command validating.
+    if (com || (com && val))  { // Check if a command and value was received.
+      res = 0; // Set it to pass and invalidate with future tests.
+      if (inputCommand.length() > 8) { // Commands wont ever be greater than 8.
+        res = 3;
+      } else {
+        testString += inputCommand; // Test if its upper case or not
+        testString.toUpperCase();
+        if (testString != inputCommand){
+          res = 3;
+          // TODO test values
+        }
+      }
+    }
+
 
     switch (res) {
-      case 0:
-        // No Error
+      case 0:   // No Error
+        if (com && val) {
+          Serial.println("!0");
+        } else {
+          Serial.println("!0 " + simVar);
+        }
         break;
-      case 3:
-        // Format Error
+      case 3:   // Format Error
+        Serial.println("!3");
         break;
-      case 4:
-        // Argument Error
+      case 4:   // Argument Error
+
         break;
-      case 5:
-        // Command Execution Impossible
+      case 5:   // Command Execution Impossible
+
         // busy for other command execution
         break;
-      case 6:
-        // Command Execution Impossible
+      case 6:   // Command Execution Impossible
+
         // mismatch in interpretation of entered command
         break;
-      default:
-        // statements
-        Serial.print("!3 "+ inputString);
+      default:  // Unhandled
+
+        if (inputCommand.length() > 0) {
+          Serial.print("!3 "+ inputString);
+        } else {
+          Serial.print("!3");
+        }
     }
 
     // clear the string:
     inputString = "";
     inputCommand = "";
     inputValue = "";
+    testString = "";
     stringComplete = false;
     separator = false;
+    val = false;
+    com = false;
+    res = 1;
   }
 }
 
@@ -103,13 +145,15 @@ void serialEvent() {
     if (inChar == '\n') {
       stringComplete = true;
     } else {
-        if (inChar == ' ') {
+        if ((inChar == ' ') && (separator == false)) {
           separator = true;
         } else {
           if (separator) {
             inputValue += inChar;
+            val = true;
           } else {
             inputCommand += inChar;
+            com = true;
           }
         }
    }
